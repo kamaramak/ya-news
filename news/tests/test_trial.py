8c27ef57626cf85f206cd889980f2074e28cfb4e
Марат Кабардоков <kabardokov.m@yandex.ru>
2025-06-17 16:38:09 +0300

    Написаны все тесты routes

--- news/tests/test_trial.py
+++ news/tests/test_trial.py
@@ -1,16 +1,16 @@
-from unittest import skip
+# from unittest import skip
 
-from django.contrib.auth import get_user_model
-from django.test import Client, TestCase
+# from django.contrib.auth import get_user_model
+# from django.test import Client, TestCase
 
-User = get_user_model()
+# User = get_user_model()
 
 
-@skip()
-class TestNews(TestCase):
+# @skip()
+# class TestNews(TestCase):
 
-    @classmethod
-    def setUpTestData(cls):
-        cls.user = User.objects.create(username='testUser')
-        cls.user_client = Client()
-        cls.user_client.force_login(cls.user)
+#     @classmethod
+#     def setUpTestData(cls):
+#         cls.user = User.objects.create(username='testUser')
+#         cls.user_client = Client()
+#         cls.user_client.force_login(cls.user)
